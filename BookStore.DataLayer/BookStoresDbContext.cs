1bc34a8def73de0e6b960a2fdd7083a83949db3b
pawel <pawel_s1@o2.pl>
2021-01-25 19:54:29 +0100

    fixed connection string and framework version

--- BookStore.DataLayer/BookStoresDbContext.cs
+++ BookStore.DataLayer/BookStoresDbContext.cs
@@ -27,7 +27,7 @@ namespace BookStore.DataLayer
         {
             //optionsBuilder.UseNpgsql(@"Host=localhost;Database=postgres;Username=postgres;Password=haselko");
             //optionsBuilder.UseInMemoryDatabase(databaseName: "InMemoryDb");
-            optionsBuilder.UseMySql("server=127.0.0.1;port=3306;user=root;password=my-secret-pw;database=database");
+            optionsBuilder.UseMySql("server=mariadb;Port=3306;user=root;password=my-secret-pw;database=database"/*, options => options.EnableRetryOnFailure()*/);
             //optionsBuilder.UseMsSql("...");
         }
     }
