e462b2a9c3504a061c05e08ed0ff987a2cd8182f
BulcziCM <kuba@codementors.pl>
2021-02-19 18:12:14 +0100

    Added WebAPI with status controller

---
+++ BookStore.WebApi/Controllers/StatusController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.WebApi.Controllers
+{
+    [Route("api/status")]
+    public class StatusController : ControllerBase
+    {
+        /*
+        Method: GET
+        URI: http://localhost:10500/api/status
+        Body: no body
+        */
+        [HttpGet]
+        public string GetStatus()
+        {
+            return "Status OK";
+        }
+    }
+}
---
+++ BookStore.WebApi/UnityDiContainerProvider.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.BusinessLayer;
+using BookStore.BusinessLayer.Serializers;
+using BookStore.DataLayer;
+using Serilog;
+using System;
+using Unity;
+using Unity.Injection;
+
+namespace BookStore.WebApi
+{
+    public class UnityDiContainerProvider
+    {
+        public IUnityContainer GetContainer()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<IBooksService, BooksService>();
+            container.RegisterType<IAuthorsService, AuthorsService>();
+            container.RegisterType<IBookStoreService, BookStoreService>();
+            container.RegisterType<IDatabaseManagementService, DatabaseManagementService>();
+            container.RegisterType<IUsersService, UsersService>();
+            container.RegisterType<INotificationsService, NotificationsService>();
+
+            container.RegisterType<INotifier, Notifier>();
+            container.RegisterType<IDataSerializersFactory, DataSerializersFactory>();
+
+            container.RegisterType<IBookRepository, BookRepository>();
+            container.RegisterType<Func<IBookStoresDbContext>>(
+                new InjectionFactory(ctx => new Func<IBookStoresDbContext>(() => new BookStoresDbContext())));
+
+            container.RegisterInstance<ILogger>(Log.Logger);
+
+            return container;
+        }
+    }
+}
